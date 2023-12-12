d2de30cb4f97afd60cb50b3fae8b85e83e850c04
Amitavkarfa <karfaamitav@gmail.com>
2023-12-12 15:07:27 +0530

    java practiece new

---
+++ hallo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class hallo{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("type our age"); 
+    int b=sc.nextInt();
+    System.out.println("you type"+b);    
+    }
+}
